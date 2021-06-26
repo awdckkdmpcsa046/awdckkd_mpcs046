5525feff6ecd6f1d0c345287efedd13255d1d441
awdckkdmpcsa046 <86476831+awdckkdmpcsa046@users.noreply.github.com>
2021-06-26 22:53:31 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,12 @@
+public class Main1
+{
+ public static void main (String args[])
+  {
+    int a = 40, b = 30;
+      a = a + b;
+      b = a - b;
+      a = a - b;
+      System.out.println ("a=" + a);
+      System.out.println ("b=" + b);
+  }
+}
